e1a350774aef17bde9379b1050f4a17c8751f1bf
Yuliya Ryabova <yuliyaryabova97@gmail.com>
2019-01-17 10:51:40 -0500

    check

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -52,7 +52,7 @@ const MainPage = props => {
             Share
           </Button>
           <Button size="small" color="primary">
-            Learn Moree
+            Learn More
           </Button>
         </CardActions>
       </Card>
